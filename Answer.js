e61a2f85f56019cb95a5b495aa0bfc182859860f
Casey Zimmer <40896016+Z1mm3r@users.noreply.github.com>
2023-05-30 11:39:48 -0400

    Update Answer.js
    Removed console.logs

--- Answer.js
+++ Answer.js
@@ -54,8 +54,6 @@ var isValidSudoku = function(board) {
     for(let i = 0; i < 3; i++){
         for(let j = 0; j < 3; j++){
            if(!isValid(getGrid(i,j,board))){
-               console.log("Failed grid")
-               console.log(i,j)
                return false;
            }
         }
